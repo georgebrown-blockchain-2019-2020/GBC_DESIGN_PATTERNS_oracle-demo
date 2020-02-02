438d0e989edf41a3a7131bb99bc93ae121baa464
leodinh <NguyenAnhTuan.Dinh@georgebrown.ca>
2020-02-02 16:37:52 -0500

    finished oracle demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import SetSymbol from "./setSymbol/setSymbol"
+import GetSymbol from "./getSymbol/getSymbol"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Set Symbol</h1>
+      <SetSymbol />
+      <h1>Get Symbol</h1>
+      <GetSymbol />
     </div>
   );
 }
